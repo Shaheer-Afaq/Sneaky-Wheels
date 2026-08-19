7df02dc1fd50b7441bc1b86ba8dfe6d7af3a9b66
Shaheer-Afaq <23121997doa@gmail.com>
2026-08-19 16:58:33 +0500

    added basic reciever and transmitter code

--- Firmware/CAM/src/main.cpp
+++ Firmware/CAM/src/main.cpp
@@ -10,8 +10,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char *ssid = "**********";
-const char *password = "**********";
+const char *ssid = "espcam";
+const char *password = "espcam123";
 
 void startCameraServer();
 void setupLedFlash();
--- Firmware/Car/src/main.cpp
+++ Firmware/Car/src/main.cpp
@@ -1,10 +1,63 @@
-#include <Arduino.h>
+// Run this first to get the MAC address of the ESP32
+/*
+#include "WiFi.h"
 
 void setup() {
-  
+
+  // Setup Serial Monitor
+  Serial.begin(115200);
+
+  // Put ESP32 into Station mode
+  WiFi.mode(WIFI_MODE_STA);
+
+  // Print MAC Address to Serial monitor
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
 }
 
 void loop() {
 
 }
+*/
 
+// Include Libraries
+#include <esp_now.h>
+#include <WiFi.h>
+
+// Define a data structure
+typedef struct struct_message {
+  char a[32];
+  int b;
+  float c;
+  bool d;
+} struct_message;
+
+// Create a structured object
+struct_message myData;
+
+
+// Callback function executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+}
+
+void setup() {
+  // Set up Serial Monitor
+  Serial.begin(115200);
+  
+  // Set ESP32 as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Initilize ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Register callback function
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+
+}
